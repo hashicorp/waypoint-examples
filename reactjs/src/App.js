d699345e52b8da5f79c9f9d32a0d9241ff0c3694
dizzyup <hector@me.com>
2020-10-06 10:44:08 +0100

    Redesign react example

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+import language from './language.svg';
+import hashi from './hashi.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  return ( 
+    <div class="container">
+      <header>
+        <a href="https://waypointproject.io" class="logo">
+          <img src={logo} />
         </a>
       </header>
+      <section class="content">
+        <div class="language-icon">
+          <img src={language} />
+        </div>
+        <h1>This React app was deployed with Waypoint.</h1>
+        <p>
+          Try making a change to this text locally and run <code>waypoint up</code> again to see it.
+        </p>
+        <p>
+          Read the <a href="https://waypointproject.io/docs">documentation</a> for more about Waypoint.
+        </p>
+      </section>
+      <footer>
+        <a href="https://hashicorp.com" class="hashi">
+          <img src={hashi} />
+        </a>
+      </footer>
     </div>
   );
 }

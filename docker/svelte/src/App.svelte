819e3d58b633c9d95b7d99333d4ba70df19fd33e
Jamie Finnigan <jfinnigan@hashicorp.com>
2020-10-12 12:22:43 -0700

    move to new design for examples

--- docker/svelte/src/App.svelte
+++ docker/svelte/src/App.svelte
@@ -1,30 +1,9 @@
 <script>
-	export let name;
+    import Index from './views/Index.svelte'
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+    <title>Waypoint Svelte Demo</title>
+</svelte:head>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Index />

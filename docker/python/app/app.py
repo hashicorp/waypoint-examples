a4f8facaeda22d04396b5d3804819a0f4c7df092
Jamie Finnigan <jfinnigan@hashicorp.com>
2020-10-12 14:11:20 -0700

    move to new design for examples

--- docker/python/app/app.py
+++ docker/python/app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "<h1 style='color:gray'>Hello, world!</h1>"
+def index():
+    return (render_template('index.html'))
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')

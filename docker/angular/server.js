c44f8a3a449c1cf0f94c66d385fca17b4f62ee68
Cody De Arkland <cody@hashicorp.com>
2020-10-12 16:10:58 -0700

    correcting hosting path in server.js

--- docker/angular/server.js
+++ docker/angular/server.js
@@ -3,9 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'dist/angular/')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('dist/angulardemo/index'))
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 

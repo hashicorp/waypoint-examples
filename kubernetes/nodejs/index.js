d8f62679bca176abc470f8e1e8a0887472fa46aa
catsby <clint@ctshryock.com>
2022-09-08 15:51:36 -0500

    update to show workspace

--- kubernetes/nodejs/index.js
+++ kubernetes/nodejs/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
+const ws = process.env.WORKSPACE || "local"
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', {workspace: ws}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

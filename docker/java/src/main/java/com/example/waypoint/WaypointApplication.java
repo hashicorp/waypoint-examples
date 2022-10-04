0f729f0eee5e0df3cfb633930a6f2e9fc21959ca
Teresa Chu <teresa.chu@hashicorp.com>
2022-10-04 14:32:37 -0400

    test

--- docker/java/src/main/java/com/example/waypoint/WaypointApplication.java
+++ docker/java/src/main/java/com/example/waypoint/WaypointApplication.java
@@ -15,7 +15,7 @@ public class WaypointApplication {
 
 	@RequestMapping("/")
 	String index() {
-		String test = "testing git polling. does this work?";
+		String test = "testing git polling. does this work? WHAT";
 		String again = "testing polling with data source. Does this work?";
 		String dev = "Once more with feeling";
 

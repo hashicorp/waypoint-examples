5d6d0bdbf3b8161af8c44760f42b0931bc0f61fe
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2023-06-28 15:02:58 -0400

    Adding an additional aws thumbprint
    per https://github.com/aws-actions/configure-aws-credentials#recent-updates

--- project-templates/ecs/advanced/terraform-org-day-zero-infra/main.tf
+++ project-templates/ecs/advanced/terraform-org-day-zero-infra/main.tf
@@ -90,7 +90,10 @@ resource "aws_iam_openid_connect_provider" "github_aws_oidc_auth_provider" {
   client_id_list = ["sts.amazonaws.com"]
 
   # Grabbed this from: https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 
 }
 
